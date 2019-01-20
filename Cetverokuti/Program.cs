67e0897c125b0ff66d413042c3e9bae74123b695
dinobaranasic <dino.baranasic8@gmail.com>
2019-01-20 18:20:21 +0100

    Pocetni zadatak

--- Cetverokuti/Program.cs
+++ Cetverokuti/Program.cs
@@ -10,6 +10,20 @@ namespace Cetverokuti
     {
         static void Main(string[] args)
         {
+            /* ZADATAK
+             * Stvoriti popis cetverokuta; programsku podršku
+             * za razne primjene gdje je potreban popis cetverokuta
+             */
+
+
+
+            /*
+             * Koliki je potencijal ovog rješenja da se jednostavno primijeni za podatke novih 10 cetverokuta?
+             * Novih 20 cetverokuta? Novih 100 cetverokuta? 1000 cetverokuta? ....
+             * Kolika je mogućnost primjene ovog rješenja uz porast količine podataka 
+             * tj. posla koji on mora odraditi? 
+             * Kolika je skalabilnost? 
+             */
         }
     }
 }
