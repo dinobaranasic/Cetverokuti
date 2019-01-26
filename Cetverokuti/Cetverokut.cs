5d1a6292916e856e3e0bc63a1799ffa57ad6bbb2
dinobaranasic <dino.baranasic8@gmail.com>
2019-01-26 15:07:35 +0100

    Sakrivanje podataka te izrada metodi za postavljanje i dohvat

--- Cetverokuti/Cetverokut.cs
+++ Cetverokuti/Cetverokut.cs
@@ -8,10 +8,48 @@ namespace Cetverokuti
 {
     class Cetverokut
     {
-        public string Vrsta;
-        public int DuzinaA;
-        public int DuzinaB;
+        //Sakrivanje podataka,onemugucanje pristupa
+        private string Vrsta;
+        private int DuzinaA;
+        private int DuzinaB;
 
+        //Metoda za postavljanje podataka "Vrsta"
+        public void PostaviVrstu(string Vrsta)
+        {
+            this.Vrsta = Vrsta;
+        }
+
+        //Metoda za dohvat podatak "Vrsta"
+        public string DohvatiVrstu()
+        {
+            return Vrsta;
+        }
+
+        //Metoda za postavljanje podataka "DuzinaA"
+        public void PostaviDuzinaA(int DuzinaA)
+        {
+            this.DuzinaA = DuzinaA;
+        }
+
+        //Metoda za dohvat podataka "DuzinaA"
+        public int DohvatiDuzinaA()
+        {
+            return DuzinaA;
+        }
+
+        //Metoda za postavljanje podataka "DuzinaB"
+        public void PostaviDuzinaB(int DuzinaB)
+        {
+            this.DuzinaB = DuzinaB;
+        }
+
+        //Metoda za dohvat podataka "DuzinaB"
+        public int DohvatiDuzinaB()
+        {
+            return DuzinaB;
+        }
+
+        //Metoda za ispis podatak "Vrsta, DuzinaA, DuzinaB"
         public void ispisVrstaDuzinaADuzinaB()
         {
             Console.WriteLine("Vrsta cetverokuta: {0}, Duzina stranice a: {1}, Duzina stranice b: {2}", Vrsta, DuzinaA, DuzinaB);
--- Cetverokuti/Program.cs
+++ Cetverokuti/Program.cs
@@ -16,14 +16,16 @@ namespace Cetverokuti
              */
 
             Cetverokut c1 = new Cetverokut();
-            c1.Vrsta = "Pravokutnik";
-            c1.DuzinaA = 3;
-            c1.DuzinaB = 4;
+            c1.PostaviVrstu("Pravokutnik");
+            c1.PostaviDuzinaA(3);
+            c1.PostaviDuzinaB(4);
+            c1.ispisVrstaDuzinaADuzinaB();
 
             Cetverokut c2 = new Cetverokut();
-            c2.Vrsta = "Pravokutnik";
-            c2.DuzinaA = 5;
-            c2.DuzinaB = 5;
+            c2.PostaviVrstu("Kvadrat");
+            c2.PostaviDuzinaA(5);
+            c2.PostaviDuzinaB(5);
+            c2.ispisVrstaDuzinaADuzinaB();
 
             List<Cetverokut> cetverokuti = new List<Cetverokut>();
             cetverokuti.Add(c1);
